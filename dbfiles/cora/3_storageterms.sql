43082689452182c5fe3dc9bca7b8c295b1e1ae4f
olovm <olov.mckie@ub.uu.se>
2022-11-25 13:28:25 +0000

    script to export data from postgresql docker

---
+++ dbfiles/cora/3_storageterms.sql
@@ -0,0 +1,16 @@
+COPY public.storageterm (id, recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+31	coraUser	coraUser:8012331835977	userUserIdStorageTerm	mican434@user.uu.se	userId
+32	coraUser	coraUser:8140805885752	userUserIdStorageTerm	malin121@user.uu.se	userId
+33	coraUser	coraUser:1986901372214875	userUserIdStorageTerm	kenan399@user.uu.se	userId
+34	coraUser	coraUser:196015452870546	userUserIdStorageTerm	perba577@user.uu.se	userId
+35	coraUser	coraUser:15697099230192	userUserIdStorageTerm	olfel499@user.uu.se	userId
+36	coraUser	coraUser:15631291678429	userUserIdStorageTerm	maken168@user.uu.se	userId
+37	coraUser	coraUser:8239436126036	userUserIdStorageTerm	stand516@user.uu.se	userId
+38	coraUser	coraUser:8199051994459	userUserIdStorageTerm	urbaeric@user.uu.se	userId
+39	coraUser	coraUser:8221860835759	userUserIdStorageTerm	marso310@user.uu.se	userId
+40	systemOneUser	141414	userUserIdStorageTerm	jsClientUser@system.cora.uu.se	userId
+41	systemOneUser	12345	userUserIdStorageTerm	guest123@system.cora.uu.se	userId
+42	systemOneUser	guest	userUserIdStorageTerm	guest123@system.cora.uu.se	userId
+44	systemOneUser	user	userUserIdStorageTerm	user@system.cora.uu.se	userId
+71	systemOneUser	121212	userUserIdStorageTerm	fitnesseUser@system.cora.uu.se	userId
+72	systemOneUser	131313	userUserIdStorageTerm	fitnesseAdmin@system.cora.uu.se	userId
