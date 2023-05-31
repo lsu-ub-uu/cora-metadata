94af9ec9d59c7c41a902f54fe21f1ea105dd32ff
perebartrolisimo <pere.bartroli@ub.uu.se>
2023-05-31 09:57:08 +0000

    update presentation for users

--- dbfiles/cora/3_storageterms.sql
+++ dbfiles/cora/3_storageterms.sql
@@ -3,14 +3,10 @@ user	121212	userUserIdStorageTerm	fitnesseUser@system.cora.uu.se	userId
 user	12345	userUserIdStorageTerm	guest123@system.cora.uu.se	userId
 user	131313	userUserIdStorageTerm	fitnesseAdmin@system.cora.uu.se	userId
 user	141414	userUserIdStorageTerm	jsClientUser@system.cora.uu.se	userId
-user	coraUser:15631291678429	userUserIdStorageTerm	maken168@user.uu.se	userId
 user	coraUser:15697099230192	userUserIdStorageTerm	olfel499@user.uu.se	userId
 user	coraUser:196015452870546	userUserIdStorageTerm	perba577@user.uu.se	userId
 user	coraUser:1986901372214875	userUserIdStorageTerm	kenan399@user.uu.se	userId
 user	coraUser:8012331835977	userUserIdStorageTerm	mican434@user.uu.se	userId
-user	coraUser:8140805885752	userUserIdStorageTerm	malin121@user.uu.se	userId
-user	coraUser:8199051994459	userUserIdStorageTerm	urbaeric@user.uu.se	userId
-user	coraUser:8221860835759	userUserIdStorageTerm	marso310@user.uu.se	userId
 user	coraUser:8239436126036	userUserIdStorageTerm	stand516@user.uu.se	userId
 user	guest	userUserIdStorageTerm	guest123@system.cora.uu.se	userId
 user	user	userUserIdStorageTerm	user@system.cora.uu.se	userId
