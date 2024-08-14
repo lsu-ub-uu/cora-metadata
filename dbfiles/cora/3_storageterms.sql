bf10891177a4cc13efb152da4bbea4760a519304
DiVA Support <diva-support@ub.uu.se>
2024-08-14 08:36:27 +0200

    DbFiles updated ons 14 aug 2024 08:36:27 CEST

--- dbfiles/cora/3_storageterms.sql
+++ dbfiles/cora/3_storageterms.sql
@@ -2,6 +2,6 @@ COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey)
 user	121212	userUserIdStorageTerm	fitnesseUser@system.cora.uu.se	userId
 user	12345	userUserIdStorageTerm	guest123@system.cora.uu.se	userId
 user	131313	userUserIdStorageTerm	fitnesseAdmin@system.cora.uu.se	userId
-user	141414	userUserIdStorageTerm	jsClientUser@system.cora.uu.se	userId
+user	141414	userUserIdStorageTerm	coraAdmin@system.cora.uu.se	userId
 user	guest	userUserIdStorageTerm	guest123@system.cora.uu.se	userId
 user	user	userUserIdStorageTerm	user@system.cora.uu.se	userId
