d96f94c5bc9922118d99dac04ce94c7ff912dc50
DiVA Support <diva-support@ub.uu.se>
2024-09-09 14:27:52 +0200

    DbFiles updated Mon Sep  9 02:27:52 PM CEST 2024

--- dbfiles/testSystem/3_storageterms.sql
+++ dbfiles/testSystem/3_storageterms.sql
@@ -1,1 +1,3 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+example	example:10093765314019722	exampleUniqueStorageTerm	unique term value	exampleUnique
+example	example:10093765314019722	exampleCombineStorageTerm	combine term value	exampleCombine
