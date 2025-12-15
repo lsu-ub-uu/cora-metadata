937c19ddbae90f05b3e8515e0bf9b4a23f607ece
DiVA Support <diva-support@ub.uu.se>
2025-12-15 14:47:48 +0100

    DbFiles updated Mon Dec 15 02:47:48 PM CET 2025

--- dbfiles/cora/3_storageterms.sql
+++ dbfiles/cora/3_storageterms.sql
@@ -3,6 +3,6 @@ user	121212	userLoginIdStorageTerm	fitnesseUser@system.cora.uu.se	loginId
 user	12345	userLoginIdStorageTerm	guest12345@system.cora.uu.se	loginId
 user	131313	userLoginIdStorageTerm	fitnesseAdmin@system.cora.uu.se	loginId
 user	141414	userLoginIdStorageTerm	systemoneAdmin@system.cora.uu.se	loginId
-user	binaryConverter	userLoginIdStorageTerm	binaryconverter@system.cora.uu.se	loginId
+user	binaryConverter	userLoginIdStorageTerm	binaryConverter@system.cora.uu.se	loginId
 user	guest	userLoginIdStorageTerm	guest@system.cora.uu.se	loginId
 user	user	userLoginIdStorageTerm	user@system.cora.uu.se	loginId
